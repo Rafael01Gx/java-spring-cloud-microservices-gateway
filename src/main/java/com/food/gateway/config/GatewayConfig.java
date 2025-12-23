a29cf603fe956376f00ffdc9ad5ef31c527c3b91
rafael01gx <rafael_junio_moraes@hotmail.com>
2025-12-23 15:26:56 -0300

    Ref: Atualizando Configs

--- src/main/java/com/food/gateway/config/GatewayConfig.java
+++ src/main/java/com/food/gateway/config/GatewayConfig.java
@@ -11,7 +11,6 @@ import static org.springframework.cloud.gateway.server.mvc.filter.LoadBalancerFi
 import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
 import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
 import static org.springframework.cloud.gateway.server.mvc.predicate.GatewayRequestPredicates.path;
-import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.*;
 
 @Configuration
 public class GatewayConfig {
